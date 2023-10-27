1efa967f9465776d26bc6eba01064e65c28c57f2
Eno <90115512+DonEnes@users.noreply.github.com>
2023-10-27 13:15:32 +0200

    Update fxmanifest.lua

--- eno_discordrichpresence/fxmanifest.lua
+++ eno_discordrichpresence/fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'adamant'
 games { 'gta5' }
 
-author 'Eno#0003'
-description 'A German Discord Rich Presence Script'
+author 'enogh'
+description 'German Discord Rich Presence Script'
 
 client_scripts {
     'main.lua'
