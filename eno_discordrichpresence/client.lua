c0effd0c00c6099146c7b1e4c2e782bceaa8258e
Eno <90115512+DonEnes@users.noreply.github.com>
2024-05-05 14:28:47 +0200

    Update client.lua

--- eno_discordrichpresence/client.lua
+++ eno_discordrichpresence/client.lua
@@ -4,7 +4,7 @@ local wait = 15000 -- nicht Anfassen :)
 
 CreateThread(function()
     while true do
-        ESX.TriggerServerCallback('masora_core:getDiscordInfos', function(cb)
+        ESX.TriggerServerCallback('eno_core:getDiscordInfos', function(cb)
             SetDiscordAppId(appid)
             SetDiscordRichPresenceAsset(image)
             SetDiscordRichPresenceAssetSmall(image)
